fa4d9bb1bb5638c5fec28d28be5413f790bb912d
LePhostavius <44902868+LePhostavius@users.noreply.github.com>
2018-11-17 14:35:38 +0100

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+
+int saisie();
+int selection_mode_jeu();
+int selection_grille();
+int switch_grille(int a);
+int switch_mode_jeu(int a);
+int switch_ia(int a);
+
+#endif // MENU_H_INCLUDED
